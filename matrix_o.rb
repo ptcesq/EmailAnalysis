b6128554e9fdad48248ea9b1c8fdd38f72863e4c
Patrick T Cronin <ptcesq@gmail.com>
2013-05-09 15:07:12 -0400

    Completed Matrix Element Change in Class

--- matrix_o.rb
+++ matrix_o.rb
@@ -1,13 +1,30 @@
 require 'matrix'
 class MatrixO
 
-  def initialize(r, c)
-     @M = Matrix.zero(r,c)
+  def initialize(row, col)
+     @M = Matrix.zero(row, col)
   end
 
-  def print_matrix()
+  def print_matrix
     @M.each_slice(@M.column_size) {|r| p r }
   end
+
+  def to_a
+    @returnArray = Array.new()
+    @M.each_slice(@M.column_size) {|r|  @returnArray.concat r }
+    return @returnArray
+  end
+
+  def set(row, col, newValue)
+    @tempArray = Array.new()
+    @M.each_slice(@M.column_size) {|r|  @tempArray << r }
+    @targetArray = @tempArray[row]
+    @targetArray[col] = newValue
+    @tempArray[row] = @targetArray
+    @M = Matrix.rows(@tempArray)
+  end
+
+
 
 
 
--- processCats.rb
+++ processCats.rb
@@ -1,7 +1,6 @@
 require_relative 'matrix_o'
 
-
 M = MatrixO.new(3,13)
 
-M.print_matrix()
+
 
