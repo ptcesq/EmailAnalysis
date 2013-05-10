d05a223595da71ad435f8ac114acc2eea1db7349
Patrick Cronin <ptcesq@gmail.com>
2013-05-10 08:54:20 -0400

    Completed Cat Process

--- processCats.rb
+++ processCats.rb
@@ -1,6 +1,31 @@
 require_relative 'matrix_o'
 
 M = MatrixO.new(3,13)
+M.print_matrix
 
 
+File.open('test.cats', 'r') do |f1|
+  while line = f1.gets
+    @values = line.split(',')
+    @row = @values[0].to_i
+    @row = @row - 1
+    @col = @values[1].to_i
+    @col = @col - 1
+    @newVal = @values[2].to_i
+    M.set(@row, @col, @newVal)
+    puts line
+    M.print_matrix
+  end
+end
 
+puts("----------------------")
+puts("Final Matrix")
+puts("")
+M.print_matrix
+
+
+puts("----------------------")
+puts("Final Array ")
+puts("")
+A = M.to_a
+A.inspect
